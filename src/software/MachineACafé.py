045949993fc1af8ad051b201ff364cc9dc79a33a
Enzo Sandré <atelier@enzosandre.fr>
2023-11-16 11:59:12 +0100

    Meilleur typage

--- src/software/MachineACafé.py
+++ src/software/MachineACafé.py
@@ -9,13 +9,13 @@ class MachineACafé:
         self.__total_encaissé = 0
         self.__hardware = hardware
 
-    def insérer(self, pièce):
+    def insérer(self, pièce: Pièce):
         if pièce.est_supérieure_ou_égale_a(PRIX_DU_CAFE):
-            code_retour_hardware = self.__hardware.declencher_ecoulement()
+            code_retour_hardware = self.__hardware.make_coffee()
             est_succes = code_retour_hardware == 0
 
             if est_succes:
                 self.__total_encaissé += pièce.valeur
 
-    def get_total_encaissé(self):
+    def get_total_encaissé(self) -> int:
         return self.__total_encaissé
--- src/software/Pièce.py
+++ src/software/Pièce.py
@@ -1,11 +1,11 @@
 class Pièce:
     __valeurs_valides = [1, 2, 5, 10, 20, 50, 100, 200]
 
-    def __init__(self, valeur):
+    def __init__(self, valeur: int):
         if valeur not in self.__valeurs_valides:
             raise ValueError()
 
         self.valeur = valeur
 
-    def est_supérieure_ou_égale_a(self, other):
+    def est_supérieure_ou_égale_a(self, other) -> bool:
         return self.valeur >= other.valeur
