65ebce8de198f937ed0c7c98ece9884aab12f5e2
Enzo Sandré <atelier@enzosandre.fr>
2023-11-16 16:05:43 +0100

    Suppression des commentaires

--- test/test_service_acceptance.py
+++ test/test_service_acceptance.py
@@ -1,7 +1,5 @@
 from software.MachineACafé import PRIX_DU_CAFE
 from software.Pièce import Pièce
-from utilities.HardwareSpy import HardwareSpy
-from utilities.HardwareStub import HardwareStub
 from utilities.MachineACaféBuilder import MachineACaféBuilder
 from utilities.serviceTestCase import ServiceTestCase
 
@@ -10,33 +8,21 @@ class TestService(ServiceTestCase):
     def test_cas_nominal(self):
         for pièce in [PRIX_DU_CAFE, Pièce(100), Pièce(200)]:
             with self.subTest(str(pièce.valeur) + "cts"):
-                # ETANT DONNE une machine a café reliée à un hardware
-                # ET une pièce d'une valeur supérieure ou égale au prix d'un café
                 machine_a_café = MachineACaféBuilder.default()
 
-                # QUAND on insère la pièce
                 machine_a_café.insérer(pièce)
 
-                # ALORS un signal d'écoulement est envoyé au hardware de la machine
                 self.assertSignalEcoulementReçuParLeHardware(machine_a_café)
-
-                # ET la pièce est encaissée
                 self.assertPièceEncaissée(machine_a_café, pièce)
 
     def test_manque_argent(self):
         for pièce in [Pièce(20), Pièce(10), Pièce(5), Pièce(2), Pièce(1)]:
             with self.subTest(str(pièce.valeur) + "cts"):
-                # ETANT DONNE une machine a café reliée à un hardware
-                # ET une pièce d'une valeur valide inférieure au prix du café
                 machine_a_café = MachineACaféBuilder.default()
 
-                # QUAND on insère la pièce
                 machine_a_café.insérer(pièce)
 
-                # ALORS aucun signal d'écoulement n'est envoyé au hardware de la machine
                 self.assertAucunSignalEcoulementReçuParLeHardware(machine_a_café)
-
-                # ET la pièce est rendue
                 self.assertAucuneSommeEncaissée(machine_a_café)
 
     def test_defaut(self):
