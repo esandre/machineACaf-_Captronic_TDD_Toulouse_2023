2980e5872b7f5c3a87a7d1b88ad1bc541dd1de28
Enzo Sandré <atelier@enzosandre.fr>
2023-11-16 10:05:35 +0100

    Refacto : ajout d'une constante

--- src/software/MachineACafé.py
+++ src/software/MachineACafé.py
@@ -1,8 +1,11 @@
+PRIX_DU_CAFE = 50
+
+
 class MachineACafé:
     def __init__(self, hardware):
         self.__hardware = hardware
 
-    def insérer(self, valeurPièceEnCentimes):
-        if valeurPièceEnCentimes >= 50:
+    def insérer(self, valeur_pièce_en_centimes):
+        if valeur_pièce_en_centimes >= PRIX_DU_CAFE:
             self.__hardware.declencher_ecoulement()
         pass
--- test/test_service.py
+++ test/test_service.py
@@ -1,6 +1,7 @@
 import unittest
 
 from software.MachineACafé import MachineACafé
+from software.MachineACafé import PRIX_DU_CAFE
 from utilities.HardwareSpy import HardwareSpy
 
 
@@ -8,7 +9,7 @@ class TestService(unittest.TestCase):
     def test_cas_nominal(self):
         # ETANT DONNE une machine a café reliée à un hardware
         # ET une pièce d'une valeur de 50cts
-        valeur_pièce_en_centimes = 50
+        valeur_pièce_en_centimes = PRIX_DU_CAFE
         hardware = HardwareSpy()
         machine_a_café = MachineACafé(hardware)
 
@@ -24,7 +25,7 @@ class TestService(unittest.TestCase):
         machine_a_café = MachineACafé(hardware)
 
         # ET une pièce d'une valeur de 49cts
-        valeur_pièce_en_centimes = 49
+        valeur_pièce_en_centimes = PRIX_DU_CAFE - 1
 
         # QUAND on insère la pièce
         machine_a_café.insérer(valeur_pièce_en_centimes)
