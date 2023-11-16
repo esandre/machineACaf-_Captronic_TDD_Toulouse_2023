8071883ed0c43a72c76f8039982f2e9d23bccc80
Enzo Sandré <atelier@enzosandre.fr>
2023-11-16 12:05:03 +0100

    Calcul des valeurs invalides à tester

--- src/software/Pièce.py
+++ src/software/Pièce.py
@@ -1,11 +1,12 @@
-class Pièce:
-    __valeurs_valides = [1, 2, 5, 10, 20, 50, 100, 200]
+VALEURS_VALIDES_EN_CENTIMES = [1, 2, 5, 10, 20, 50, 100, 200]
 
-    def __init__(self, valeur: int):
-        if valeur not in self.__valeurs_valides:
+
+class Pièce:
+    def __init__(self, valeur_en_centimes: int):
+        if valeur_en_centimes not in VALEURS_VALIDES_EN_CENTIMES:
             raise ValueError()
 
-        self.valeur = valeur
+        self.valeur = valeur_en_centimes
 
     def est_supérieure_ou_égale_a(self, other) -> bool:
         return self.valeur >= other.valeur
--- test/test_pièce_unit.py
+++ test/test_pièce_unit.py
@@ -1,18 +1,30 @@
 import random
 import unittest
 
-from software.Pièce import Pièce
+from software.Pièce import Pièce, VALEURS_VALIDES_EN_CENTIMES
 
 
-class MyTestCase(unittest.TestCase):
+class TestValeurPièces(unittest.TestCase):
+    @classmethod
+    def valeurs_invalides_testées(cls):
+        # Test des limites supérieures et inférieures des valeurs valides
+        for valeurValide in VALEURS_VALIDES_EN_CENTIMES:
+            valeur_supérieure = valeurValide + 1
+            if valeur_supérieure not in VALEURS_VALIDES_EN_CENTIMES:
+                yield valeur_supérieure
+
+            valeur_inférieure = valeurValide - 1
+            if valeur_inférieure not in VALEURS_VALIDES_EN_CENTIMES:
+                yield valeur_inférieure
+
     def test_valeurs_valides(self):
-        for valeur in [1, 2, 5, 10, 20, 50, 100, 200]:
+        for valeur in VALEURS_VALIDES_EN_CENTIMES:
             with self.subTest(str(valeur) + 'cts'):
                 piece = Pièce(valeur)
                 self.assertEqual(valeur, piece.valeur)
 
     def test_valeurs_invalides(self):
-        for valeur in [0, 3, 4, 6, 9, 11, 19, 21, 49, 51, 99, 101, 199, 201]:
+        for valeur in self.__class__.valeurs_invalides_testées():
             with self.subTest(str(valeur) + 'cts'):
                 with self.assertRaises(ValueError):
                     Pièce(valeur)
