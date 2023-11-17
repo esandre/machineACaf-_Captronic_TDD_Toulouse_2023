cd9fb0a51aa1d9f80958053263d91b3131fe19c1
Enzo Sandré <atelier@enzosandre.fr>
2023-11-17 11:59:08 +0100

    Enrichissement du HardwareSpy

--- test/utilities/HardwareSpy.py
+++ test/utilities/HardwareSpy.py
@@ -5,14 +5,21 @@ from hardware.HardwareInterface import HardwareInterface
 
 class HardwareSpy(HardwareInterface):
     def __init__(self, spied: HardwareInterface):
-        self.make_coffee_appelé = False
+        self.__make_product_invocations = []
         self.__decorated = spied
 
-    def make_product(self) -> int:
-        self.make_coffee_appelé = True
+    def make_product(self,
+                     coffee: bool = True,
+                     more_water: bool = False,
+                     milk: bool = False,
+                     chocolate: bool = False) -> int:
+        self.__make_product_invocations.append((coffee, more_water, milk, chocolate))
         return self.__decorated.make_product()
 
     def register_money_inserted_callback(self, callback: Callable[[int], bool]):
         pass
+
+    def make_product_appelé(self):
+        return len(self.__make_product_invocations) > 0
 
 
