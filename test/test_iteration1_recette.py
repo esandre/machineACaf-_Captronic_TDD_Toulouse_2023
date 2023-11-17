568a956c68d26abd4dce3360491c2068e7d2666f
Enzo Sandré <atelier@enzosandre.fr>
2023-11-17 16:12:29 +0100

    Exemple de test de recette

---
+++ test/test_iteration1_recette.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestItération1(unittest.TestCase):
+    def test_recette(self):
+        # Servir café allongé en CB
+        # Servir latté avec 2€ en pièce
+        # Essayer de servir capuccino alors que machine en défaut
+        # Appuyer sur latté, capuccino, allongé puis à nouveau latté, mettre 1€
+        # Demander allongé avec 5cts
+        pass
